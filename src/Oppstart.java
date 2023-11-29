4175d1d61e4583d969f34ad5db72ac6ef2e09c4c
h669791 <669791@stud.hvl.no>
2023-11-29 10:54:23 +0100

    Dette er en test for å sjekke om github push funker gjennom Intellij
    29.11.2023

---
+++ src/Oppstart.java
@@ -0,0 +1,11 @@
+public class Oppstart {
+
+    public static void main(String[] args) {
+
+        String navn = "Jonas, Anday, Elias";
+        String test = "Velkommen til vårt nye spennende prosjekt";
+        System.out.println(navn);
+        System.out.println(test);
+
+    }
+}
